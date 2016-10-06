db7cd2b487711be6872cd916a96799f70c59e5f1
Jacob Wilson <jacob@Jacobs-MacBook-Pro.local>
2016-10-05 19:40:51 -0500

    Initial Commit
    I made a test app to have on the repo

---
+++ BraveLife/BraveLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BraveLife
+//
+//  Created by Jacob Wilson on 10/5/16.
+//  Copyright © 2016 BUFusion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
